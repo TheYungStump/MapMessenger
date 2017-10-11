d84221fbec2aa29f010dda60dca8f340ef7cced5
Lisna Vargheese <vlisna789@gmail.com>
2017-10-11 09:27:49 -0400

    added admin and user verification

--- app/src/main/java/com/example/vlisn/m4/User.java
+++ app/src/main/java/com/example/vlisn/m4/User.java
@@ -14,16 +14,17 @@ public class User {
     public String name;
     public String email;
     public String password;
-
+    public boolean access;
     // Default constructor required for calls to
     // DataSnapshot.getValue(User.class)
     public User() {
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, Boolean access) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.access = access;
     }
     public String getEmail() { return email;}
     public String getPassword() { return password;}
