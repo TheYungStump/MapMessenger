22f9b659f613555537e1b0c00887e8ec790b206c
dyagna98 <dyagnamurthy3@gatech.edu>
2017-10-25 03:20:42 -0400

    WHY WONT THIS WORK

--- app/src/main/java/com/example/vlisn/m4/RatRegister.java
+++ app/src/main/java/com/example/vlisn/m4/RatRegister.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by Divya on 10/25/2017.
@@ -13,6 +19,22 @@ import android.widget.Button;
 public class RatRegister extends AppCompatActivity implements View.OnClickListener{
 
     Button addB;
+    EditText borough= (EditText) findViewById(R.id.borough);
+    String boroughVal = borough.getText().toString();
+    EditText city = (EditText) findViewById(R.id.city);
+    String cityVal = city.getText().toString();
+    EditText address= (EditText) findViewById(R.id.incidentAddress);
+    String addressVal = address.getText().toString();
+    EditText zip= (EditText) findViewById(R.id.incidentZip);
+    String zipVal = zip.getText().toString();
+    EditText location= (EditText) findViewById(R.id.locationType);
+    String locationVal = location.getText().toString();
+    EditText createData = (EditText) findViewById(R.id.createData);
+    String createDataVal = createData.getText().toString();
+    EditText latitude= (EditText) findViewById(R.id.latitude);
+    String latitudeVal = latitude.getText().toString();
+    EditText longitude= (EditText) findViewById(R.id.longitude);
+    String longitudeVal = longitude.getText().toString();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +42,35 @@ public class RatRegister extends AppCompatActivity implements View.OnClickListen
 
         addB = (Button) findViewById(R.id.add);
         addB.setOnClickListener(this);
+        System.out.println("HI");
     }
 
     public void onClick(View v) {
+        System.out.println("reached add");
         switch (v.getId()) {
             case R.id.add:
-                System.out.println("reached add button");
+                try {
+                    System.out.println("reached add button");
+                    File file = new File("addedRats.csv");
+                    // if file doesnt exists, then create it
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write(createDataVal);
+                    bw.write(locationVal);
+                    bw.write(zipVal);
+                    bw.write(addressVal);
+                    bw.write(cityVal);
+                    bw.write(boroughVal);
+                    bw.write(latitudeVal);
+                    bw.write(longitudeVal);
+                    bw.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(this, RatRegister.class));
         }
 
