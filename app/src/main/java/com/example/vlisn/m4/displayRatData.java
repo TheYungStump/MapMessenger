bbae780ac5b18073de68b4410bd5d703dd735e99
Lisna Vargheese <vlisna789@gmail.com>
2017-11-01 08:18:11 -0400

    database done

--- app/src/main/java/com/example/vlisn/m4/displayRatData.java
+++ app/src/main/java/com/example/vlisn/m4/displayRatData.java
@@ -25,7 +25,7 @@ import java.util.List;
  * Created by Divya on 10/17/2017.
  */
 
-public class displayRatData extends AppCompatActivity{
+public class displayRatData extends AppCompatActivity {
 
     int args = RatData.args;
     ArrayAdapter<String> adapter;
@@ -33,6 +33,7 @@ public class displayRatData extends AppCompatActivity{
 
     /**
      * displays information about each key upon clicking on it
+     *
      * @param savedInstanceState bundle object that is passed on creation
      */
     public void onCreate(Bundle savedInstanceState) {
@@ -44,82 +45,33 @@ public class displayRatData extends AppCompatActivity{
         mFirebaseInstance1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (int i = 0; i <= args; i++) {
-                            HashMap ratkeysdate = (HashMap) dataSnapshot.getValue();
-                            System.out.println("args "+ args);
-                            System.out.println("i "+ i);
-                            System.out.println("ratdate "+ ratkeysdate);
-                            if(i == args) {
-                                System.out.println("ratdate "+ ratkeysdate);
-                                String x = ratkeysdate.toString() + "%rats";
-                                ratDatalist.add(x.substring(0, x.indexOf("%")));
-                                break;
-                            }
+                for (int i = 0; i <= args; i++) {
+                    HashMap ratkeysdate = (HashMap) dataSnapshot.getValue();
+                    System.out.println("args " + args);
+                    System.out.println("i " + i);
+                    System.out.println("ratdate " + ratkeysdate);
+                    if (i == args) {
+                        System.out.println("ratdate " + ratkeysdate);
+                        String x = ratkeysdate.toString() + "%rats";
+                        ratDatalist.add(x.substring(0, x.indexOf("%")));
+                        break;
                     }
+                }
 
                 adapter = new ArrayAdapter<String>(displayRatData.this,
                         R.layout.activity_listview, ratDatalist);
-                ratDates.setAdapter(adapter);;
+                ratDates.setAdapter(adapter);
+                ;
 
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 throw databaseError.toException(); // don't ignore errors
             }
         });
     }
+}
 
 
-
-
-
-
-                                                 }
-
-
-
-//                for (int i = 0; i < args; i++) {
-//                    nextNextLine = reader.readNext();
-//                }
-//                //displays the required data
-//                if (nextNextLine[1] != null) {
-//                    ratList.add(nextLine[1] + ": " + nextNextLine[1]);
-//                } else {
-//                    ratList.add(nextLine[1] + ": " + " ");
-//                }
-//                if (nextNextLine[7] != null) {
-//                    ratList.add(nextLine[7] + ": " + nextNextLine[7]);
-//                } else {
-//                    ratList.add(nextLine[7] + ": " + " ");
-//                }
-//                if (nextNextLine[8] != null) {
-//                    ratList.add(nextLine[8] + ": " + nextNextLine[8]);
-//                } else {
-//                    ratList.add(nextLine[8] + ": " + " ");
-//                }
-//                if (nextNextLine[9] != null) {
-//                    ratList.add(nextLine[9] + ": " + nextNextLine[9]);
-//                } else {
-//                    ratList.add(nextLine[9] + ": " + " ");
-//                }
-//                if (nextNextLine[16] != null) {
-//                    ratList.add(nextLine[16] + ": " + nextNextLine[16]);
-//                } else {
-//                    ratList.add(nextLine[16] + ": " + " ");
-//                }
-//                if (nextNextLine[23] != null) {
-//                    ratList.add(nextLine[23] + ": " + nextNextLine[23]);
-//                } else {
-//                    ratList.add(nextLine[23] + ": " + " ");
-//                }
-//                if (nextNextLine[24] != null) {
-//                    ratList.add(nextLine[24] + ": " + nextNextLine[24]);
-//                } else {
-//                    ratList.add(nextLine[24] + ": " + " ");
-//                }
-//                if (nextNextLine[25] != null) {
-//                    ratList.add(nextLine[25] + ": " + nextNextLine[25]);
-//                } else {
-//                    ratList.add(nextLine[25] + ": " + " ");
-//                }
 
